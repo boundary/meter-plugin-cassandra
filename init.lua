dfc615390315f9f5061c161c7ec5622bbcb5f9d9
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-09-17 20:34:33 +0000

    Added support for querying multiple MBeans.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -18,6 +18,11 @@ local DataSource = framework.DataSource
 local CommandOutputDataSource = framework.CommandOutputDataSource
 local params = framework.params
 local clone = framework.table.clone
+local gsplit = framework.string.gsplit
+local split = framework.string.split
+local notEmpty = framework.string.notEmpty
+local pack = framework.util.pack
+local ipack = framework.util.ipack
 local os = require('os')
 
 local JMXDataSource = CommandOutputDataSource:extend()
@@ -25,21 +30,35 @@ function JMXDataSource:initialize(options)
   options.path = 'java'
   options.use_popen = true
   local args  = ('-jar jmxquery.jar -U "service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi"'):format(options.host, options.port)
-  options.args = { args .. ' -O "java.lang:type=Memory" -A "NonHeapMemoryUsage" -K used' }
+  local mbeans = table.concat(options.mbeans, ';') 
+  options.args = { args .. ' -O "java.lang:type=Memory" -A "NonHeapMemoryUsage" -K committed -X "' .. mbeans .. '"' }
+
   CommandOutputDataSource.initialize(self, options)
 end
 
 local options = clone(params)
+options.mbeans = {
+   'java.lang:type=Memory|NonHeapMemoryUsage|used',
+   'java.lang:type=Memory|NonHeapMemoryUsage|init',
+   'java.lang:type=Memory|NonHeapMemoryUsage|max'
+}
 
-
-
-
+local metric_mapping = {
+}
 
 local dataSource = JMXDataSource:new(options)
 local plugin = Plugin:new(params, dataSource)
 
 function plugin:onParseValues(data)
-  p(data.output, os.time())
+  local result = {}
+  local lines = split(data.output, '\n');
+  for _, l in pairs(lines) do
+    if notEmpty(l) then
+      local metric, value = l:match('JMX OK%s*-%s*([%p%w]+)=(%d+) ')
+      ipack(result, metric, value)
+    end
+  end
+  return result
 end
 
 plugin:run()
