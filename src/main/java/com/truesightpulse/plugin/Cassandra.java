4d0e9d02ed5f963978752785e9775de65c6601a3
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-09-22 14:32:04 -0300

    JMXPlugin base configuration.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- src/main/java/com/truesightpulse/plugin/Cassandra.java
+++ src/main/java/com/truesightpulse/plugin/Cassandra.java
@@ -20,7 +20,7 @@ import com.boundary.plugin.sdk.PluginRunner;
 public class Cassandra {
 
     public static void main(String[] args) {
-        PluginRunner plugin = new PluginRunner("com.boundary.plugin.sdk.examples.SimplePlugin");
+        PluginRunner plugin = new PluginRunner("com.boundary.plugin.sdk.jmx.JMXPlugin");
         plugin.run();
     }
 
